19f7e46f5f7e5bcb3aedb487926990351a928008
Barissa-Imran <imranbarissa@gmail.com>
2022-10-17 00:12:38 +0300

    Task 8 - Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse,
+ * main - print all numbers of base 16 in lowercase,
  * followed by a newline, using putchar
  * Return: Always 0 (success)
  */
 int main(void)
 {
+	int n;
 	char ch;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar('\n');
+	putchar('\n')
 	return (0);
 }
