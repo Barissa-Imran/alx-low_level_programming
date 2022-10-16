ba3b82e3494c60a3bb74c4c978ec770c971fd464
Barissa-Imran <imranbarissa@gmail.com>
2022-10-17 00:35:53 +0300

    Task 11 - The success combination in business is: Do you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(',');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
+}
