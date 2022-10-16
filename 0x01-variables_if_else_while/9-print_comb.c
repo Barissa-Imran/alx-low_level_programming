c9d970910390844fab626f37b129952995673e74
Barissa-Imran <imranbarissa@gmail.com>
2022-10-17 00:19:30 +0300

    Task 9 - Patience, persistence an aperspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - print all numbers of base 16 in lowercase,
- * followed by a newline, using putchar
+ * main - print all possible combinations of single-digit numbers
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
-	char ch;
 
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-	}
-	for (ch = 'a'; ch <= 'f'; ch++)
-	{
-		putchar(ch);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
