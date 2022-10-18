0d46d598d4d43e96168639bf3f890090cf2895ca
Barissa-Imran <imranbarissa@gmail.com>
2022-10-19 00:03:49 +0300

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print _putchar, followed by a newline
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+# Welcome to 0x02-functions_nested_loops
+
+`0-putchar.c` - Prints _putchar , followed by a new line.
+* The program should return 0
