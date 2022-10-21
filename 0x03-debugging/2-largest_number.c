066f2242482c5c979adcca159a29feef3c45f904
Barissa-Imran <imranbarissa@gmail.com>
2022-10-22 01:11:47 +0300

    Fix code errors

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * largest number - prints the largest three numbers
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
@@ -10,16 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = b;
+	int largest;
 
-	if (a > b)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (c > b)
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,17 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap year in account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: 0
- */
+ * * print_remaining_days - takes a date and prints how many days are
+ * * left in the year, taking leap years into account
+ * * @month: month in number format
+ * * @day: day of month
+ * * @year: year
+ * * Return: void
+*/
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
