384e7eb8f9b4784d45969c0a86a4a84f1d4bee3f
Imran <imranbarissa@gmail.com>
2023-01-17 16:58:05 +0300

    refresh dir

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -c -fPIC *.c
-gcc -shared -o liball.so *.o
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=/tmp/iwin.so
