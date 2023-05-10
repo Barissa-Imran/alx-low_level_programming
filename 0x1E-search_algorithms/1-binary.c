7524369fb8b62ab6dc1f8d341fc040f7dc0ce01d
Barissa-Imran <imranbarissa@gmail.com>
2023-05-10 23:01:23 +0300

    Add task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,5 @@
-#incude "search_algos.h"
-#include <stddef.h>
+#include "search_algos.h"
+
 /**
  * binary_search - Searches for a value in a sorted array
  * of intergers using binary search.
@@ -13,7 +13,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t i, left, right;
 
-	if (array == Null)
+	if (array == NULL)
 		return (-1);
 
 	for (left = 0, right = size - 1; right >= left;)
